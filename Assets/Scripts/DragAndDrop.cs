a6da0ab634a1b9e8fbe72a4eca3e630a254bbd9c
Lin Ting Chieh <lintingchieh@Lins-MacBook-Pro.local>
2020-04-14 20:01:44 +1000

    first commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    public Canvas canvas;
+    private CanvasGroup canvasGroup;
+    
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = .5f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // To change anchore position
+        //Make sure scale is fit / canvas.scaleFactor
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+}
+
+//IPointerDownHandler => When you click it
+//IBeginDragHandler => When you start the position
+//IEndDragHandler => When you end the postion
+//IDragHandler => Move the object during time
---
+++ Assets/Scripts/DropScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class DropScript : MonoBehaviour, IDropHandler
+{
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            //Get Circle position and make it the same as the square
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
+                GetComponent<RectTransform>().anchoredPosition;
+            score += 10;
+            scoreText.text = "Score + " + score;
+        }
+    }
+}
